eccabd0305577b8a10500e3d6b3bee28d0160586
Travis Veazey <travisvz@gmail.com>
2023-03-24 22:10:33 -0800

    Add probabilities for onsets/codas and clusters

--- src/lib.rs
+++ src/lib.rs
@@ -85,6 +85,8 @@ impl From<SyllableBuilder> for Syllable {
             nucleus_clusters: value.nucleus_clusters,
             codas: value.codas,
             coda_clusters: value.coda_clusters,
+
+            ..Default::default()
         }
     }
 }
@@ -96,17 +98,29 @@ pub struct Syllable {
     nucleus_clusters: Vec<Grapheme>,
     codas: Vec<Grapheme>,
     coda_clusters: Vec<Grapheme>,
+
+    probability_onset_exists: f64,
+    probability_onset_is_cluster: f64,
+    probability_nucleus_is_cluster: f64,
+    probability_coda_exists: f64,
+    probability_coda_is_cluster: f64,
 }
 
 impl Default for Syllable {
     fn default() -> Self {
         Self {
             onsets: vec![ "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" ].into_iter().map(|s| s.into()).collect(),
-            onset_clusters: vec![], // vec![ "ch", "sh", "bl", "cl", "fl", "pl", "gl", "br", "cr", "dr", "pr", "tr", "th", "sc", "sp", "st", "sl", "spr" ].into_iter().map(|s| s.to_owned()).collect(),
+            onset_clusters: vec![ "ch", "sh", "bl", "cl", "fl", "pl", "gl", "br", "cr", "dr", "pr", "tr", "th", "sc", "sp", "st", "sl", "spr" ].into_iter().map(|s| s.into()).collect(),
             nucleus: vec![ "a", "e", "i", "o", "u" ].into_iter().map(|s| s.into()).collect(),
-            nucleus_clusters: vec![], // vec![ "ae", "ea", "ai", "ia", "au", "ay", "ie", "oi", "ou", "ey" ].into_iter().map(|s| s.to_owned()).collect(),
+            nucleus_clusters: vec![ "ae", "ea", "ai", "ia", "au", "ay", "ie", "oi", "ou", "ey" ].into_iter().map(|s| s.into()).collect(),
             codas: vec![ "b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "r", "s", "t", "v", "x", "y"].into_iter().map(|s| s.into()).collect(),
-            coda_clusters: vec![], // vec![ "ck", "st", "sc", "ng", "nk", "rsh", "lsh", "rk", "rst", "nct", "xt" ].into_iter().map(|s| s.to_owned()).collect(),
+            coda_clusters: vec![ "ck", "st", "sc", "ng", "nk", "rsh", "lsh", "rk", "rst", "nct", "xt" ].into_iter().map(|s| s.into()).collect(),
+
+            probability_onset_exists: 0.95,
+            probability_onset_is_cluster: 0.25,
+            probability_nucleus_is_cluster: 0.25,
+            probability_coda_exists: 0.10,
+            probability_coda_is_cluster: 0.25,
         }
     }
 }
@@ -121,26 +135,31 @@ impl Syllable {
 
         let mut syllable = String::new();
 
-        let onset = if self.onset_clusters.is_empty() || rng.gen_bool(0.5) {
-            self.onsets.choose(&mut rng).unwrap()
-        } else {
-            self.onset_clusters.choose(&mut rng).unwrap()
-        };
-        syllable.push_str(onset.as_ref());
+        if rng.gen_bool(self.probability_onset_exists) {
+            let onset = if self.onset_clusters.is_empty() || !rng.gen_bool(self.probability_onset_is_cluster) {
+                self.onsets.choose(&mut rng).unwrap()
+            } else {
+                self.onset_clusters.choose(&mut rng).unwrap()
+            };
+            syllable.push_str(onset.as_ref());
+        }
 
-        let nucleus = if self.nucleus_clusters.is_empty() || rng.gen_bool(0.5) {
+        // TODO: Should there be a probability that a nucleus exists?
+        let nucleus = if self.nucleus_clusters.is_empty() || !rng.gen_bool(self.probability_nucleus_is_cluster) {
             self.nucleus.choose(&mut rng).unwrap()
         } else {
             self.nucleus_clusters.choose(&mut rng).unwrap()
         };
         syllable.push_str(nucleus.as_ref());
 
-        let coda = if self.coda_clusters.is_empty() || rng.gen_bool(0.5) {
-            self.codas.choose(&mut rng).unwrap()
-        } else {
-            self.coda_clusters.choose(&mut rng).unwrap()
-        };
-        syllable.push_str(coda.as_ref());
+        if rng.gen_bool(self.probability_coda_exists) {
+            let coda = if self.coda_clusters.is_empty() || !rng.gen_bool(self.probability_coda_is_cluster) {
+                self.codas.choose(&mut rng).unwrap()
+            } else {
+                self.coda_clusters.choose(&mut rng).unwrap()
+            };
+            syllable.push_str(coda.as_ref());
+        }
 
         syllable
     }
